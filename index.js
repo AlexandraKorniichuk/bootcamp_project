7b7d2bc512bd0b9edc80746db6cea53991ed0390
korniichuk_alex <angel.win04@gmail.com>
2020-06-03 14:12:42 +0300

    arrows reaction & list seeing & change places of elem

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 "use strict";
+
+let crlsRoot = document.querySelector(".ant-carousel");
+console.log(crlsRoot);
+
+let crlsList = document.querySelector(".ant-carousel-list");
+console.log(crlsList);
+
+let crlsElements = document.querySelectorAll(".ant-carousel-element");
+console.log(crlsElements);
+
+let crlsElemFirst = document.querySelector(".ant-carousel-element");
+console.log(crlsElemFirst);
+
+let leftArrow = document.querySelector("div.ant-carousel-arrow-left");
+console.log(leftArrow);
+
+let rightArrow = document.querySelector("div.ant-carousel-arrow-right");
+console.log(rightArrow);
+
+let indicatorDots = document.querySelector(".ant-carousel-dots");
+console.log(indicatorDots);
+
+let elm;
+elm = crlsList.lastElementChild;
+console.log("последний елемент списка", elm);
+
+let buf;
+buf = elm.cloneNode(true);
+console.log("ключ последнего елемента списка", buf);
+
+crlsList.insertBefore(buf, crlsList.firstElementChild);
+
+crlsList.removeChild(elm);
+console.log(crlsList);
+
+leftArrow.addEventListener("click", leftArrowClick);
+rightArrow.addEventListener("click", rightArrowClick);
+
+function leftArrowClick() {
+    alert("Сработал клик на левую стрелку");
+}
+
+function rightArrowClick() {
+    alert("Сработал клик на правую стрелку");
+}
